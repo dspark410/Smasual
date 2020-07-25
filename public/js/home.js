17884ec33ddb57f57883ec402278e0ee9deb03e3
tanyap <biddanda.tanya@gmail.com>
2020-07-25 01:08:22 -0700

    adds working navbars, simple CSS to /Home and email modal

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,10 @@
 
 $(document).ready(() => {
 
-    $.get("/api/profile_data").then((data) => {
-        console.log("data: "+data)
-      $(".user-profile").text(data.firstName);
-    });
+    // $.get("/api/profile_data").then((data) => {
+    //     console.log("data: "+data)
+    //   $(".user-profile").text(data.firstName);
+    // });
 
 
     
