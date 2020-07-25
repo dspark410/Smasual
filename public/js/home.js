13f25cff1397a0abbaac51061752fcd6766cd88b
mistahpig <dspark410@gmail.com>
2020-07-25 11:04:06 -0700

    final changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,19 @@
 
-$(document).ready(() => {
-    const UserID = localStorage.getItem("UserId")
+// $(document).ready(() => {
+//     const UserID = localStorage.getItem("UserId")
 
 
-    $.get("/api/profile_data/"+UserID).then((data) => {
-        console.log("data: "+JSON.stringify(data))
-      $(".user-name").text(data.firstName);
-      $(".user-age").text(data.age);
-      $(".user-gender").text(data.gender);
-      $(".user-orientation").text(data.genderOrientation);
-      $(".user-bio").text(data.biography);
-    });
+//     $.get("/api/profile_data/"+UserID).then((data) => {
+//         console.log("data: "+JSON.stringify(data))
+//       $(".user-name").text(data.firstName);
+//       $(".user-age").text(data.age);
+//       $(".user-gender").text(data.gender);
+//       $(".user-orientation").text(data.genderOrientation);
+//       $(".user-bio").text(data.biography);
+//     });
 
 
     
 
-});
+// });
 
